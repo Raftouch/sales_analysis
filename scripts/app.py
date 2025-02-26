762c712014d70131765f543162a92da56dae5a74
raftouch <ntimorshina@gmail.com>
2025-02-26 21:31:11 +0100

    create db, get urls, response 2OO

--- scripts/app.py
+++ scripts/app.py
@@ -1,4 +1,17 @@
 import sqlite3
+import requests
+
+urlSales = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSawI56WBC64foMT9pKCiY594fBZk9Lyj8_bxfgmq-8ck_jw1Z49qDeMatCWqBxehEVoM6U1zdYx73V/pub?gid=760830694&single=true&output=csv"
+urlProducts = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSawI56WBC64foMT9pKCiY594fBZk9Lyj8_bxfgmq-8ck_jw1Z49qDeMatCWqBxehEVoM6U1zdYx73V/pub?gid=0&single=true&output=csv"
+urlStores = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSawI56WBC64foMT9pKCiY594fBZk9Lyj8_bxfgmq-8ck_jw1Z49qDeMatCWqBxehEVoM6U1zdYx73V/pub?gid=714623615&single=true&output=csv"
+
+responseSales = requests.get(urlSales)
+responseProducts = requests.get(urlProducts)
+responseStores = requests.get(urlStores)
+
+print(responseSales)
+print(responseProducts)
+print(responseStores)
 
 connection = sqlite3.connect('data/sales.db')
 
@@ -36,12 +49,12 @@ sales_per_store(id INTEGER PRIMARY KEY, quantity_sold INTEGER, revenue_per_store
 create_table_sales_per_product = """CREATE TABLE IF NOT EXISTS
 sales_per_product(id INTEGER PRIMARY KEY, quantity_sold INTEGER, revenue_per_product DECIMAL(10,2), product_id INTEGER, FOREIGN KEY (product_id) REFERENCES products(id))"""
 
-cursor.execute(create_table_products)
-cursor.execute(create_table_stores)
-cursor.execute(create_table_sales)
-cursor.execute(create_table_total_revenue)
-cursor.execute(create_table_sales_per_store)
-cursor.execute(create_table_sales_per_product)
+# cursor.execute(create_table_products)
+# cursor.execute(create_table_stores)
+# cursor.execute(create_table_sales)
+# cursor.execute(create_table_total_revenue)
+# cursor.execute(create_table_sales_per_store)
+# cursor.execute(create_table_sales_per_product)
 
 connection.commit()
 connection.close()
